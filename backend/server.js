28136a481b69a0df7aa45112008aa948e0429512
anonymous8528 <152532887+anonymous8528@users.noreply.github.com>
2026-04-13 12:56:35 +0530

    Fix CORS and PORT

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,26 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://anonymous8528.github.io",
+      "https://anonymous8528.github.io/citi-care-"
+    ],
+    credentials: true,
+  }
+
+  ));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/issues", issueRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log("Server running")
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
  console.log("MONGO_URI:", process.env.MONGO_URI);
